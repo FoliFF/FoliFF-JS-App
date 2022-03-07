bc2ba4fe9ef6726c045c9325d382333adefc09e7
FoliFF <olof@hisham.se>
2022-03-07 22:02:51 +0100

    Update to add a pokemonlist
    Removed all previous code to add an array list which contains a list of Pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-alert('Hello world');
+/* A list of pokemon that I choose from the website https://pokedex.org/ */
 
-var favoriteFood = 'Chicken Curry';
-
-document.write('My Favorite food is ' + favoriteFood + ' as well as ');
-
-favoriteFood = 'Steaks';
-
-document.write(favoriteFood);
+let pokemonList = [{
+    name: 'Quilava',
+    height: 0.9,
+    type: ['Fire']
+},
+{
+    name: 'Lugia',
+    height: 5.2,
+    type: ['Psychic', 'Flying']
+},
+{
+    name: 'Kabutops',
+    height: 1.3,
+    type: ['Water', 'Rock']
+},
+{
+    name: 'Jolteon',
+    height: 0.8,
+    type: ['Electric']
+}];
