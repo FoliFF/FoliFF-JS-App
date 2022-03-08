3205b95cf3faafc83712b63ae86bdbaaa2fa474c
FoliFF <olof@hisham.se>
2022-03-08 20:03:56 +0100

    Compare pokemon hight
    Created a for-loop with if-condition to check and compare pokemon heights and print out which one is biggest and smallest in the list.

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,23 @@ let pokemonList = [{
     height: 0.8,
     type: ['Electric']
 }];
+
+/*For-loop that will print out the pokemonList array on the page.*/
+ 
+for(let i = 0; i < pokemonList.length; i++)
+{
+    /*Checks if any in the pokemonList height is over 5. If it is it will add It's big! */
+    if(pokemonList[i].height > 5){
+        
+        document.write(pokemonList[i].name + ' (Hight: ' + pokemonList[i].height + ' m) Wow that is big! </br>');
+    }
+    /*Checks the smallest in the pokemonList. If it is it will add It's small */
+    else if (pokemonList[i].height < 0.9){
+        document.write(pokemonList[i].name + ' (Hight: ' + pokemonList[i].height + ' m) Wow that is smaller than I expected! </br>');
+    }
+    /* Otherwise print out their height and name */
+    else{ 
+        document.write(pokemonList[i].name + ' (Hight: ' + pokemonList[i].height + ' m) </br>');
+    }
+    
+}
